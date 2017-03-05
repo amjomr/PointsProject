6a6bf298a335329c7f329ed2a9ddff0d56586617
amjomr <aallobadi@lcmail.lcsc.edu>
2017-03-04 16:06:51 -0800

    Adding RequireHttps

--- PointsProject/Controllers/HomeController.cs
+++ PointsProject/Controllers/HomeController.cs
@@ -6,8 +6,10 @@ using System.Web.Mvc;
 
 namespace PointsProject.Controllers
 {
+    [RequireHttps]
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
