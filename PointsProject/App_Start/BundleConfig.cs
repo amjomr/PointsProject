221dcc06e1fd0a2f8f8f37c09f586af3e40d888b
amjomr <aallobadi@lcmail.lcsc.edu>
2017-03-05 17:18:38 -0800

    Adding new CSS Style

--- PointsProject/App_Start/BundleConfig.cs
+++ PointsProject/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace PointsProject
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumin.css",
                       "~/Content/site.css"));
         }
     }
