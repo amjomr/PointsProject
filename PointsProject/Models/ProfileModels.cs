49ef1b959755c46dd03f6371631cc78705e517d4
student <student@MLH240-09>
2017-04-26 10:56:43 -0700

    Profile Model values put in

--- PointsProject/Models/ProfileModels.cs
+++ PointsProject/Models/ProfileModels.cs
@@ -7,5 +7,20 @@ namespace PointsProject.Models
 {
     public class ProfileModels
     {
+        //Info about them (Table: Points)
+        public string email { get; set; }
+        public int totalPoints { get; set; }
+        public int availablePoints { get; set; }
+
+        //where they have been or where they have swiped their card (Table: checkins)
+        public DateTime date { get; set; }
+        public string location { get; set; }
+        public float period { get; set; }
+
+        //Displaying what they have purchased (Table: purchases)
+        public string item { get; set; }
+        public int pointsSpent { get; set; }
+
+
     }
 }
